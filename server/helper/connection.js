9a03b9ad876299551d78100dc1773f0417c0f40b
Ananth B S <57173817+ananthbs@users.noreply.github.com>
2024-04-07 23:45:13 +0530

    Update connection.js

--- server/helper/connection.js
+++ server/helper/connection.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 var mysqlConnection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Wednesday@365',
+    password: 'password',
     database: 'CrawlDB'
 });
 
