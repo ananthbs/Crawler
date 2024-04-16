6c59234e85cb9686dabcf324124a778fbde62809
ananth.bs <ananth.bs@ANANTH-BS-02-MAC.local>
2024-04-16 18:24:44 +0530

    changes crawlcomponent to class

--- client/src/home/App.jsx
+++ client/src/home/App.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import ClientView from './components/ClientView';
-import CrawlComponent from './components/CrawlComponent';
+// import CrawlComponent from './components/CrawlComponent';
 import Loader from './components/Loader';
+import Crawler from './components/CrawlComponent';
+import {getAllClientsData, startCrawling } from './actions/home.action';
 
 const App = () => {
-  
+  const dispatch = useDispatch();
  const {isFetchingData } = useSelector((state) => state);
  console.log(isFetchingData)
   return (
     <>
+    <Crawler getAllClientsData={getAllClientsData} startCrawling={startCrawling} dispatch={dispatch}/>
       {isFetchingData && <Loader/>}
-      <CrawlComponent/>
+      {/* <CrawlComponent/> */}
       <ClientView/>
     </>
   )
--- client/src/home/components/CrawlComponent.jsx
+++ client/src/home/components/CrawlComponent.jsx
@@ -1,55 +1,81 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Box, Heading, Input, Button, useToast, Flex, Divider } from '@chakra-ui/react';
 import { useDispatch, useSelector } from 'react-redux';
-import { startCrawling } from '../actions/home.action';
-import { useEffect } from 'react';
+import {getAllClientsData, startCrawling } from '../actions/home.action';
+import { connect } from "react-redux";
 
-const CrawlComponent = () => {
-    const {crawlingStatus ,isCrawling } = useSelector((state) => state);
-    const dispatch = useDispatch();
-  const [size, setSize] = useState('');
-  const toast = useToast();
+class Crawler extends React.Component {
+constructor(props){
+  super(props);
+  this.props = props;
+}
 
-  const onClickCrawl = (event) => {
-    event.preventDefault();
-    dispatch(startCrawling(size));
-    toast({
-        title: "crawling started it will take sometime",
-        description: `crawling and adding ${size} data to DB`,
-        status: "success",
-        duration: 2000,
-        isClosable: true,
-      });
-  };
+ 
+
+componentDidMount(){
+  this.onClickCrawl();
+  console.log(this.props)
+}
+
+componentWillReceiveProps(newProps){
+  debugger
+  if(!this.props.state?.crawlingStatus?.success && newProps.state?.crawlingStatus?.success){
+    this.props.dispatch(this.props.getAllClientsData())
+  }
+}
+
+onClickCrawl(){
+  this.props.dispatch(this.props.startCrawling(10));
+  // toast({
+  //     title: "crawling started it will take sometime",
+  //     description: `crawling and adding ${10} data to DB`,
+  //     status: "success",
+  //     duration: 2000,
+  //     isClosable: true,
+  //   });
+};
+
+  render(){
+    // const {crawlingStatus ,isCrawling } = this.props.useSelector((state) => state);
+    // const dispatch = useDispatch();
+  // const [size, setSize] = useState('10');
+  // const toast = useToast();
+
+  // useEffect(() => {
+  //   onClickCrawl()
+  // },[]);
+
+  
 
 
-  useEffect(() =>{
-    if(crawlingStatus && crawlingStatus.message){
-        if(crawlingStatus.success){
-            toast({
-                title: "crawling done successfully",
-                description: crawlingStatus.message,
-                status: "success",
-                duration: 3000,
-                isClosable: true,
-              });
-        }
-        else{
-            toast({
-                title: "crawling failed",
-                description: crawlingStatus.message,
-                status: "error",
-                duration: 3000,
-                isClosable: true,
-              });
-        }
-    }
-  },[crawlingStatus]);
+  // useEffect(() =>{
+  //   if(crawlingStatus && crawlingStatus.message){
+  //       if(crawlingStatus.success){
+  //           toast({
+  //               title: "crawling done successfully",
+  //               description: crawlingStatus.message,
+  //               status: "success",
+  //               duration: 3000,
+  //               isClosable: true,
+  //             });
+  //             dispatch(getAllClientsData()); 
+  //       }
+  //       else{
+  //           toast({
+  //               title: "crawling failed",
+  //               description: crawlingStatus.message,
+  //               status: "error",
+  //               duration: 3000,
+  //               isClosable: true,
+  //             });
+  //       }
+  //   }
+  // },[crawlingStatus]);
 
   return (
     <Box textAlign="center" p={8} bg="gray.100" borderRadius="md">
-      <Heading as="h1" mb={6} fontSize="xl">Clicking below Button will scrape all the data, which will take long time to avoid that please restrict the number of data</Heading>
-      <form onSubmit={onClickCrawl}>
+      {/* <Heading as="h1" mb={6} fontSize="xl">Clicking below Button will scrape all the data, which will take long time to avoid that please restrict the number of data</Heading> */}
+      {/* <form onSubmit={onClickCrawl}>
         <Flex alignItems={"center"} gap="1rem" justifyContent={"center"} mb="4rem">
           <Input
             type="number"
@@ -62,10 +88,19 @@ const CrawlComponent = () => {
           />
           <Button type="submit" colorScheme="blue" size="lg" disabled={isCrawling}>Crawl</Button>
         </Flex>
-      </form>
+      </form> */}
       <Divider colorScheme={"grey.500"} border=".5px solid grey"/>
     </Box>
   );
+  }
 };
 
-export default CrawlComponent;
+
+
+const mapStateToProps = (state) => ({
+  state: state
+});
+
+const mapDispatchToProps = {  };// not able to add actions as they were async, addition of thunk fails other files
+
+export default connect(mapStateToProps, mapDispatchToProps)(Crawler);
